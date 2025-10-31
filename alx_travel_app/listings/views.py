b5f31bc46c0e5ca00b6c4565589653f7fef457e1
Maha065 <maha060506@gmail.com>
2025-10-31 21:06:25 +0100

    Add BookingViewSet with email confirmation
    Implement BookingViewSet for managing bookings and sending confirmation emails.

---
+++ alx_travel_app/listings/views.py
@@ -0,0 +1,14 @@
+from rest_framework import viewsets
+from .models import Booking
+from .serializers import BookingSerializer
+from .tasks import send_booking_confirmation_email
+
+class BookingViewSet(viewsets.ModelViewSet):
+    queryset = Booking.objects.all()
+    serializer_class = BookingSerializer
+
+    def perform_create(self, serializer):
+        booking = serializer.save()
+        # Trigger background email task
+        if booking.customer.email:
+            send_booking_confirmation_email.delay(booking.customer.email, booking.id)
